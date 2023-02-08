e4324350df0d898e5ea9640961b909c9232c3012
William Tichenor <williamtichenor@tamu.edu>
2023-02-07 22:10:34 -0600

    Finale

--- functions.js
+++ functions.js
@@ -1,11 +1,17 @@
-function balls() {
+function setCSS() {
+	if (localStorage.getItem("currStyle")==2) document.getElementById("css").href="style2.css";
+}
+
+function switchCSS() {
 	var currCSS = document.getElementById("css");
 	if (currCSS.getAttribute("href")=="style.css")
 	{
+		localStorage.setItem("currStyle",2);
 		document.getElementById("css").href="style2.css";
 	}
 	else
 	{
+		localStorage.setItem("currStyle",1);
 		document.getElementById("css").href="style.css";
 	}
 }
